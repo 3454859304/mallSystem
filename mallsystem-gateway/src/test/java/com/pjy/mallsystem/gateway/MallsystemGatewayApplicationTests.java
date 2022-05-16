42fff42bb138a9181936e66ab16bdee8f5dce7dc
pjy <3454859304@qq.com>
2022-05-16 20:05:55 +0800

    重构父工程依赖

---
+++ mallsystem-gateway/src/test/java/com/pjy/mallsystem/gateway/MallsystemGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjy.mallsystem.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallsystemGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
